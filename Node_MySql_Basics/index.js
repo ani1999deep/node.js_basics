6767ae87ace68db9999c1490e72557c575619f84
Anideep1 <anideepbhowmick1@gmail.com>
2024-04-21 14:28:42 +0530

    new files are added

--- Node_MySql_Basics/index.js
+++ Node_MySql_Basics/index.js
@@ -1,6 +1,26 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql2");
 
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "12345678",
+  database: "nodemysql",
+  port: 3000,
+  multipleStatements: true,
+});
+
+//Connected to database
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.log(err, "Error");
+  } else {
+    console.log("Connection is ON!");
+  }
+});
+
+//Port Define
 const server = app.listen(3000, () => {
   console.log("Server is running...");
 });
